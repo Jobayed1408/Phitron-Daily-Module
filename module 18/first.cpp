83bd1415b621f834c9b65beb9d3e76acc3392ebf
Jobayed1408 <mdjobayedislam4@gmail.com>
2023-01-23 22:55:24 +0600

    new file

---
+++ module 18/first.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#define ll long long ;
+//long long int memo[1000];
+vector<long long int>memo(1000,-1);
+long long int fib(int n)
+{
+      
+}
+
+int main()
+{
+//      memset(memo, -1, sizeof(memo));
+      int n;
+      cin>>n;
+      cout<<fib(n);
+}
